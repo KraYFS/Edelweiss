0a4c4f39e58541ad2b1534111bddd6ea4a585f13
KraYFS <grigorievdaniil13@gmail.com>
2024-12-17 00:08:11 +0200

    added mainpage&header

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This is a project that will be used for commercial purposes written in 
 - # React + Vite
+- # i18react
+- # SwiperJS
---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,11 @@
+import styles from './MainPage.module.css'
+
+const MainPage = () => {
+    return (
+        <div className={styles.mainPage}>
+            main page
+        </div>
+    );
+}
+
+export default MainPage;
