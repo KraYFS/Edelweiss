f28f797afa49af4f5c7b3095ba7e68e8bc7bbda5
KraYFS <grigorievdaniil13@gmail.com>
2024-12-26 03:37:39 +0200

    fix: changed name of imported img

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,7 +4,7 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import styles from './MainPage.module.css'
-import img from '../../assets/categoryitem.png'
+import img from '../../assets/categoryItem.png'
 
 const MainPage = () => {
     return (
@@ -79,7 +79,7 @@ const MainPage = () => {
                     <a href='#' className={styles.category_house_item}>
                         <img className={styles.category_house_item_img} src={img} alt="" />
                         <h2 className={styles.category_house_item_title}>
-                            Готовые одноэтажные дома 
+                            Готовые одноэтажные дома
                             <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
                                 <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
