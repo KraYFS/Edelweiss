fb8a84e4e01c7921fbe909ee2e726a6ca1583070
KraYFS <grigorievdaniil13@gmail.com>
2024-12-26 03:28:31 +0200

    added new block

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,6 +4,7 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import styles from './MainPage.module.css'
+import img from '../../assets/categoryitem.png'
 
 const MainPage = () => {
     return (
@@ -23,6 +24,71 @@ const MainPage = () => {
                     <MainPageCard />
                 </SwiperSlide>
             </Swiper>
+            <div className="container">
+                <div className={styles.category_house}>
+                    <a href='#' className={styles.category_house_item}>
+                        <img className={styles.category_house_item_img} src={img} alt="" />
+                        <div className={styles.category_house_item_title}>
+                            Готовые одноэтажные дома
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
+                                <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
+                            </svg>
+                        </div>
+                    </a>
+                    <a href='#' className={styles.category_house_item}>
+                        <img className={styles.category_house_item_img} src={img} alt="" />
+                        <div className={styles.category_house_item_title}>
+                            Готовые двухэтажные дома
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
+                                <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
+                            </svg>
+                        </div>
+                    </a>
+                    <a href='#' className={styles.category_house_item}>
+                        <img className={styles.category_house_item_img} src={img} alt="" />
+                        <div className={styles.category_house_item_title}>
+                            Готовые дома с терассой
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
+                                <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
+                            </svg>
+                        </div>
+                    </a>
+                    <a href='#' className={styles.category_house_item}>
+                        <img className={styles.category_house_item_img} src={img} alt="" />
+                        <div className={styles.category_house_item_title}>
+                            Готовые дома с гаражем
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
+                                <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
+                            </svg>
+                        </div>
+                    </a>
+                    <a href='#' className={styles.category_house_item}>
+                        <img className={styles.category_house_item_img} src={img} alt="" />
+                        <div className={styles.category_house_item_title}>
+                            Готовые бани
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
+                                <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
+                            </svg>
+                        </div>
+                    </a>
+                    <a href='#' className={styles.category_house_item}>
+                        <img className={styles.category_house_item_img} src={img} alt="" />
+                        <div className={styles.category_house_item_title}>
+                            Готовые одноэтажные дома 
+                            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 8L14.2759 8" stroke="white" strokeWidth="2" strokeLinejoin="bevel" />
+                                <path d="M8.48267 1.48279L14.9999 8.00003L8.48267 14.5173" stroke="white" strokeWidth="2" />
+                            </svg>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
         </div>
     );
 }
