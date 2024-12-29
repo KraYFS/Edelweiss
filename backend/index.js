42c5637c49c307f38ce44628fb3d08445c035dbb
KraYFS <grigorievdaniil13@gmail.com>
2024-12-29 04:02:10 +0200

    added backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express"
+import mongoose from "mongoose";
+import cors from 'cors'
+import router from "./Routers/router.js";
+
+const PORT = 3000;
+const DB_URL = 'mongodb+srv://edelweissBack:edelweissBackPass@cluster0.txcsr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+const app = express()
+
+app.use(cors({
+    origin: ['http://localhost:5173']
+}))
+app.use(express.json())
+app.use('/api', router)
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, () => console.log(`Server started: localhost:${PORT}`))
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+startApp()
