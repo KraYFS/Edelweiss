0aab39fdeab054c00d8f5d0d6aaf30ade834a65f
Danil Grigoriev <105729789+KraYFS@users.noreply.github.com>
2025-06-03 14:38:46 +0300

    test

--- frontend/src/layouts/MainLayout/index.tsx
+++ frontend/src/layouts/MainLayout/index.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../../components/Header'
 
 const MainLayout = () => {
   return (
     <>
+      <Header />
       <Suspense fallback='тут компонент загрузки'>
         <Outlet />
       </Suspense>
